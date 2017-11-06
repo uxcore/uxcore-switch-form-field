efb4ad4d68116507cd0c71ef78d65be16cb142a1
shumi.lg <shumi.lg@alibaba-inc.com>
2017-11-06 14:14:14 +0800

    cjs to es6 module, remove useless props from Switch.

--- src/SwitchFormField.js
+++ src/SwitchFormField.js
@@ -5,11 +5,11 @@
  * Copyright 2015-2016, Uxcore Team, Alinw.
  * All rights reserved.
  */
-const React = require('react');
-const Switch = require('uxcore-switch');
-const FormField = require('uxcore-form-field');
-const assign = require('object-assign');
-const Constants = require('uxcore-const');
+import React from 'react'
+import Switch from 'uxcore-switch'
+import FormField from 'uxcore-form-field'
+import assign from 'object-assign'
+import Constants from 'uxcore-const'
 
 class SwitchFormField extends FormField {
 
@@ -29,8 +29,20 @@ class SwitchFormField extends FormField {
 
   renderField() {
     const me = this;
+    const switchUselessPropKeys = [`jsxlabel`, `verticalAlign`, `labelMatchInputHeight`,
+      `jsxshow`, `jsxshowLabel`, `jsxprefixCls`, `jsxflex`, `jsxname`, `jsxplaceholder`,
+      `jsxtips`, `standalone`, `jsxinstant`, `jsxVerticalAlign`, `jsxsize`, `asyncValidate`,
+      `totalFlex`, `attachFormField`, `detachFormField`, `handleDataChange`, `getValues`, `resetValues`];
     const switchProps = {...me.props};
+
+    switchUselessPropKeys.forEach(key => {
+      if (switchProps.hasOwnProperty(key)) {
+        delete switchProps[key]
+      }
+    })
+
     const mode = me.props.jsxmode || me.props.mode;
+
     if (mode === Constants.MODE.EDIT) {
       return (
         <Switch {...switchProps} checked={me.state.value} onChange={me.handleChange.bind(me)} style={{}} className="" />
